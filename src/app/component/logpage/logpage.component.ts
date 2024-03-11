c293942fbc63a1b86ba4b305436a064eed132f10
Gabriel Sesta <gabrielsesta3@gmail.com>
2024-03-11 09:51:17 +0100

    instaurazione progetto e creazione logpage component

---
+++ src/app/component/logpage/logpage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logpage',
+  standalone: true,
+  imports: [],
+  templateUrl: './logpage.component.html',
+  styleUrl: './logpage.component.sass'
+})
+export class LogpageComponent {
+
+}
